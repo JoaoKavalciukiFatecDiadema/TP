028aa77fe6d345a8c987c1b9f0d5b868b3a4aa65
JoaoFaculdade <joao.sanches7@fatec.sp.gov.br>
2022-10-12 23:06:01 -0300

    Teste atividade08

---
+++ atividade08/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno joaoPedro = new Aluno("Joao Pedro", 17.0f, 20.0f, 15.0f);
+        System.out.println(joaoPedro);
+    }
+}
